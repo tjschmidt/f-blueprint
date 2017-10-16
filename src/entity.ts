aaa9dbb28a62094cc79415682affd0ef3c0863e0
schmidttj <schmidttj@leidos.com>
2017-10-16 11:31:37 -0700

    Updated Entity to reference and draw a Sprite

--- src/entity.ts
+++ src/entity.ts
@@ -1,12 +1,29 @@
 class Entity {
     private static currentId = 1;
+
     readonly id: number;
-    private x: number;
-    private y: number;
+    x: number;
+    y: number;
+    private sprite: Sprite;
+
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
+        this.sprite = null;
         this.id = Entity.currentId;
         ++Entity.currentId;
     }
+
+    setSprite(sprite: Sprite): Entity {
+        this.sprite = sprite;
+        return this;
+    }
+
+    draw(context: CanvasRenderingContext2D, x: number, y: number, width: number, height: number): Entity {
+        if (this.sprite !== null) {
+            this.sprite.draw(context, x, y, width, height);
+            return this;
+        }
+    }
+
 }
