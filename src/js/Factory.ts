04bfac51c016079f29515b0eac9627f4d753e08b
schmidttj <schmidttj@leidos.com>
2017-10-16 14:14:39 -0700

    Created Library and Factory classes

---
+++ src/js/Factory.ts
@@ -0,0 +1,25 @@
+class Factory<K, T> {
+    private library: Library<() => T>;
+
+    constructor(readonly keyFn: (key: K) => number = null) {
+        this.library = new Library<() => T>(100);
+    }
+
+    add(key: K, constructor: () => T): void {
+        this.library.add(constructor, this.getKey(key));
+    }
+
+    create(key: K): T {
+        let constructor = this.library.get(this.getKey(key));
+        if (constructor === null)
+            return null;
+        return constructor();
+    }
+
+    private getKey(key: K): number {
+        if (typeof key === 'number')
+            return key;
+        return this.keyFn(key);
+    }
+
+}
---
+++ src/js/Library.ts
@@ -0,0 +1,29 @@
+class Library<T> {
+    private storage: T[];
+
+    constructor(capacity: number, readonly keyFn: (arg: T) => number = null) {
+        this.storage = new Array(capacity);
+    }
+
+    add(item: T, key: number = null): void {
+        if (key === null) {
+            if (this.keyFn === null)
+                throw Error('Must provide a key function');
+            key = this.keyFn(item);
+        }
+        if (key >= this.storage.length)
+            this.resize(2 * this.storage.length);
+        this.storage[key] = item;
+    }
+
+    get(key: number): T {
+        let val = this.storage[key];
+        if (val === undefined)
+            return null;
+        return val;
+    }
+
+    private resize(size: number): void {
+        this.storage.length = size;
+    }
+}
