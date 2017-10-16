1f8d5a9ce1c20b1950211a384d0b6bf433e17cb7
schmidttj <schmidttj@leidos.com>
2017-10-16 11:32:05 -0700

    Added various utility functions

---
+++ src/util.ts
@@ -0,0 +1,22 @@
+function binarySearch(array: any[], key: any, keyFn: (arg: any) => any): number {
+    return _binarySearch(array, key, keyFn, 0, array.length - 1);
+}
+
+function _binarySearch(array: any[], key: any, keyFn: (arg: any) => any, low: number, high: number): number {
+    if (low >= high)
+        return -1;
+    let mid = Math.floor((high + low) / 2);
+    let value = keyFn(array[mid]);
+    if (value === key)
+        return mid;
+    if (value < key)
+        return _binarySearch(array, key, keyFn, mid + 1, high);
+    return _binarySearch(array, key, keyFn, low, mid - 1);
+}
+
+function strokeLine(context: CanvasRenderingContext2D, sx: number, sy: number, dx: number, dy: number) {
+    context.beginPath();
+    context.moveTo(sx, sy);
+    context.lineTo(dx, dy);
+    context.stroke();
+}
