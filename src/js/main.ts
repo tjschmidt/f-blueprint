a704ed4471b007a576eec94a329090de62d87e35
schmidttj <schmidttj@leidos.com>
2017-10-16 14:17:41 -0700

    Added a couple example files for testing

---
+++ src/js/main.ts
@@ -0,0 +1,23 @@
+let spriteSheet = new SpriteSheet('spritesheet.png');
+
+let spriteLibrary = new Library<Sprite>(100, (sprite: Sprite) => {
+    return sprite.type;
+});
+spriteLibrary.add(new Sprite(spriteSheet, 0, 0, 16, 16, SpriteType.Black));
+spriteLibrary.add(new Sprite(spriteSheet, 16, 0, 16, 16, SpriteType.Blue));
+spriteLibrary.add(new Sprite(spriteSheet, 0, 16, 16, 16, SpriteType.Green));
+spriteLibrary.add(new Sprite(spriteSheet, 16, 16, 16, 16, SpriteType.Red));
+
+let entityFactory = new Factory<EntityType, Entity>();
+entityFactory.add(EntityType.Black, function () {
+    return new Entity(EntityType.Black, spriteLibrary.get(SpriteType.Black))
+});
+entityFactory.add(EntityType.Blue, function () {
+    return new Entity(EntityType.Blue, spriteLibrary.get(SpriteType.Blue))
+});
+entityFactory.add(EntityType.Green, function () {
+    return new Entity(EntityType.Green, spriteLibrary.get(SpriteType.Green))
+});
+entityFactory.add(EntityType.Red, function () {
+    return new Entity(EntityType.Red, spriteLibrary.get(SpriteType.Red))
+});
